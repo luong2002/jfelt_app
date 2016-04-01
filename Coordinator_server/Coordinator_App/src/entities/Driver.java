fac22f5173e1726435590011c8b830f639f0f0e3
Felipe Izepe <felipeizepe@gmail.com>
2016-03-31 17:18:47 -0700

    Base of the Project
    Simple application where a message is send to the server from an app and
    also the Software Requirement Specification

---
+++ Coordinator_server/Coordinator_App/src/entities/Driver.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class Driver {
+
+	private String name;
+	private String ID;
+	
+	public Driver(String name, String ID) {
+		this.name = name;
+		this.ID = ID;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getID() {
+		return ID;
+	}
+	
+}
---
+++ Coordinator_server/Coordinator_App/src/entities/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author felipe
+ *
+ */
+package entities;
